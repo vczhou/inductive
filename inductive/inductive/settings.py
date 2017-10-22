b8cb843cb79eb4009b4d2a459721b933ca71d928
Stephen Ma <stephenma064@gmail.com>
2017-10-21 20:16:00 -0500

    heroku edit

--- inductive/inductive/settings.py
+++ inductive/inductive/settings.py
@@ -129,11 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 SITE_ID = 3
